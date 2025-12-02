cce3706ac91462fc9d0bf85d1dcb80332c100dcf
fuadhasan199 <fuadhasan740266@gmail.com>
2025-12-02 20:29:57 +0600

    sessionId

--- src/Extra-Component/Payment/PaymentSucces.jsx
+++ src/Extra-Component/Payment/PaymentSucces.jsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSearchParams } from 'react-router';
+import useAxios from '../../Auth/useAxios';
 
-const PaymentSucces = () => {
+const PaymentSucces = () => { 
+    const[searchParams,]=useSearchParams()  
+    const axiosSecure=useAxios()
+    const sessionId=searchParams.get('session_id') 
+    console.log(sessionId) 
+
+  useEffect(()=>{
+   if(sessionId){
+       axiosSecure.patch(`/payment-success?session_id=${sessionId}`) 
+       .then(res=>{
+        console.log(res.data)
+       })
+   }
+
+
+  },[sessionId,axiosSecure])
+
+
     return (
         <div>
             <h1 className='mt-5 text-center text-5xl'>Your payment Succesfull </h1>
